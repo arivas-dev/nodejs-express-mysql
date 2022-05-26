3c8a3a2e8c5aa207cf0a81777e58dac745de15e9
Jonathan Rivas <jonathan.rivas@grupocassa.com>
2022-05-26 08:12:15 -0600

    Deploy to heroku

--- app/routes/loan.routes.js
+++ app/routes/loan.routes.js
@@ -1,22 +1,23 @@
+const {ensureAuthenticated} = require('../middlewares/auth.middleware.js')
 module.exports = app => {
     const loan = require("../controllers/loan.controller.js");
   
     var router = require("express").Router();
   
     // // Create a new loan
-    router.post("/loanBook", loan.loanBook);
+    router.post("/loanBook",ensureAuthenticated, loan.loanBook);
   
     // // Retrieve all loans
-    router.get("/", loan.getAll);
+    router.get("/",ensureAuthenticated, loan.getAll);
   
     // // Retrieve all published loans
     // router.get("/published", loans.findAllPublished);
   
     // // Retrieve a single loan with id
-    router.get("/:id", loan.findByUser);
+    router.get("/:id", ensureAuthenticated,loan.findByUser);
 
 
-    router.put("/:id", loan.returnBook);
+    router.put("/:id",ensureAuthenticated, loan.returnBook);
   
     // // Update a loan with id
     // router.put("/:id", loans.update);
